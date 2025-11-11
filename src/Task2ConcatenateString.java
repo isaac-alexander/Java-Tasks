bb5490df162b0f6e72eb8fd5e8cb63a1e26a47aa
Isaac Alexander <Isaacalexander81@gmail.com>
2025-11-11 02:18:28 +0100

    src/Task2ConcatenateString.java

--- src/Task2ConcatenateString.java
+++ src/Task2ConcatenateString.java
@@ -1,27 +1,31 @@
-//Write a Java program to concatenate a given string to the end of another string.
+// Write a Java program to concatenate a given string to the end of another string.
 // Sample Output:
-// String 1: Java Exercises and
-// String 2: Python Exercises
 // Result: Java Exercises and Python Exercises
 
 // Steps / method:
-// Assign the string characters to a variable
-// used the concat() method to concatenate to build a sentence with both text and variables
+// Use the concat() method to concatenate to build a sentence with both text and variables
 
-
-
+// Declare and assign both strings to variables
+// Call the method to combine the strings
+// Print the returned result
 
 public class Task2ConcatenateString {
-    public static void main(String[] args) {
-        // Assign the string characters to a variable
 
+    // method to join two strings
+    public static String joinStrings(String text1, String text2) {
+        return text1.concat(text2); // concatenate text2 to text1
+    }
+
+    public static void main(String[] args) {
+
+        // Declare and assign strings
         String text1 = "Java Exercises and ";
         String text2 = "Python Exercises";
 
-        //used the concat method to concatenate text1 to text2 and assign to a result variable
-        String result = text1.concat(text2);
+        // Call the method and store result
+        String result = joinStrings(text1, text2);
 
-        //print the result
+        // Print result
         System.out.println(result);
     }
 }
