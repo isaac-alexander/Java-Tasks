ed2e7c01e47201113042f28fa8c5547c859d31ce
Isaac Alexander <Isaacalexander81@gmail.com>
2025-11-14 02:23:56 +0100

    src/Task13SimpleInterestProgram.java

--- src/Task13SimpleInterestProgram.java
+++ src/Task13SimpleInterestProgram.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+// Write a Program to Calculate Simple Interest in Java
+//Input :  P = 10000  R = 5  T = 5
+//Output : 2500
+
+// Steps / method:
+
 
 public class Task13SimpleInterestProgram {
 }
