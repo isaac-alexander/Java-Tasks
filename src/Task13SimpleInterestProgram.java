f88a7a4332ea99176665901f43926b7d8743ed2e
Isaac Alexander <Isaacalexander81@gmail.com>
2025-11-15 02:23:54 +0100

    src/Task13SimpleInterestProgram.java

--- src/Task13SimpleInterestProgram.java
+++ src/Task13SimpleInterestProgram.java
@@ -6,4 +6,16 @@
 
 
 public class Task13SimpleInterestProgram {
+
+
+    // this method will calculate simple interest using the formula (P * R * T) / 100
+// it takes 3 integer parameters: principal, rate, and time
+    public static int calculateSimpleInterest(int principal, int rate, int time) {
+        // Multiply principal by rate and store the result in a variable called step1
+        // Multiply step1 by time and store the result in a variable called step2
+        // Divide step2 by 100 to get the final simple interest value
+        // Return the simple interest value
+        return 0; // dummy return so the code can compile
+    }
+
 }
