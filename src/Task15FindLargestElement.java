abbebfa1e5ce74c174427bddb6188ff5eb097b28
Isaac Alexander <Isaacalexander81@gmail.com>
2025-11-15 02:24:11 +0100

    src/Task15FindLargestElement.java

--- src/Task15FindLargestElement.java
+++ src/Task15FindLargestElement.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+// Write a Java Program to Find the Largest Element in Array
+// Input: [ 7, 2, 5, 1, 4]
+// Output: 7
+
+
+
 
 public class Task15FindLargestElement {
+
+    // this method takes an integer array param called arr
+    public static int findLargest(int[] arr) {
+        // Create a variable called max and set it to the first element
+        // Loop through the array
+        // If a number is bigger than max, update max
+        // Return max after loop ends
+        return 0; // dummy return
+    }
+
 }
