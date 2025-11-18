11ef522447a56947ff4292c75aef66fb1067163e
Isaac Alexander <Isaacalexander81@gmail.com>
2025-11-18 01:49:53 +0100

    src/Task20CheckNumber.java

--- src/Task20CheckNumber.java
+++ src/Task20CheckNumber.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+// Write a Java program that reads an integer and check whether it is negative, zero, or positive.
+//Input number: 35
+//Expected Output : Number is positive
+
 
 public class Task20CheckNumber {
+
+    // This method takes an integer input called num
+    // It will check whether the number is positive, negative, or zero
+    // Then it returns a String describing the result
+    public static String checkNumberType(int num) {
+        // If num > 0
+        // Return "Number is positive"
+        // Else if num < 0
+        // Return "Number is negative"
+        // Else
+        // Return "Number is zero"
+        return ""; // dummy return to compile
+    }
+
+    public static void main(String[] args) {
+        // Assign number to a variable called number
+
+        // Call checkNumberType with number as argument
+        // Store returned string in result
+
+        // Print result to console
+    }
 }
