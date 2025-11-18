5ed91e856368ae2e63a7b18d8ed4dd0fba7412b1
Isaac Alexander <Isaacalexander81@gmail.com>
2025-11-18 01:49:39 +0100

    src/Task19ReverseStringProgram.java

--- src/Task19ReverseStringProgram.java
+++ src/Task19ReverseStringProgram.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+// Java String Program to Reverse a String
+//Input: str= "Geeks"
+//Output: "skeeG"
 
 public class Task19ReverseStringProgram {
+
+    public static String reverseString(String str) {
+        // Create an empty string that will store the reversed result
+        // Loop through the original string from the last letter to the first letter
+        // During each loop, take the current character and add it to the reversed string
+        // After the loop ends, return the reversed string
+        return ""; // dummy return so the method compiles
+    }
+
 }
