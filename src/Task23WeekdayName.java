ce508b028b2737e10afc4abd1b3c959cdcf60e57
Isaac Alexander <Isaacalexander81@gmail.com>
2025-11-18 01:50:42 +0100

    src/Task23WeekdayName.java

--- src/Task23WeekdayName.java
+++ src/Task23WeekdayName.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+// Write a Java program that takes a number from the user and generates an integer between 1 and 7. It displays the weekday name.
+// Input number: 3
+// Expected Output : Wednesday
 
 public class Task23WeekdayName {
+
+    // This method takes an integer dayNumber
+    // It returns the corresponding weekday name as a String
+    public static String getWeekday(int dayNumber) {
+        // Use switch-case to match dayNumber from 1 to 7
+        // 1 -> "Monday", 2 -> "Tuesday", ..., 7 -> "Sunday"
+        // If dayNumber is not between 1-7, return "Invalid day number"
+        return ""; // dummy return
+    }
+
+    public static void main(String[] args) {
+        // Assign an integer number
+
+        // Call getWeekday with the number
+        // Store returned value in a variable
+
+        // Print the weekday
+    }
 }
