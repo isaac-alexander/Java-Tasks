3567b8c8cd552503304df034d60e7d114df1beec
Isaac Alexander <Isaacalexander81@gmail.com>
2025-11-19 02:06:05 +0100

    src/Task28CalculateSpeed.java

--- src/Task28CalculateSpeed.java
+++ src/Task28CalculateSpeed.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+
 
 public class Task28CalculateSpeed {
+
+    // This method accepts distance in meters, and time in hours, minutes, seconds
+    public static void calculateSpeed(double distance, int hours, int minutes, int seconds) {
+
+        // Convert hours, minutes, seconds into total seconds
+        // Compute speed in meters/second
+        // Compute speed in kilometers/hour
+        // Compute speed in miles/hour (divide meters by 1609 to convert to miles)
+        // Print all three speeds
+    }
+
+    public static void main(String[] args) {
+        // distance, hours, minutes and seconds
+        // Call calculateSpeed()
+    }
 }
