306639c9b85c7e830e45af0a5be22bd667978d9b
Isaac Alexander <Isaacalexander81@gmail.com>
2025-11-19 02:05:20 +0100

    src/Task25CompareNumbers.java

---
+++ src/Task25CompareNumbers.java
@@ -0,0 +1,28 @@
+// Write a Java program that accepts three numbers and prints "All numbers are equal" if all three numbers are equal, "All numbers are different" if all three numbers are different and "Neither all are equal or different" otherwise.
+// Input first number: 2564
+// Input second number: 3526
+// Input third number: 2456
+// Expected Output : All numbers are different
+
+
+public class Task25CompareNumbers {
+    // This method takes 3 integers: a, b, c
+    // It determines if:
+    // 1. All three are equal
+    // 2. All three are different
+    // 3. Or neither condition
+    public static String checkNumbers(int a, int b, int c) {
+        // If a equals b AND b equals c return "All numbers are equal"
+        // Else if all three numbers are different from each other return "All numbers are different"
+        // Otherwise return "Neither all are equal or different"
+        return ""; // dummy return
+    }
+
+    public static void main(String[] args) {
+        // three numbers
+
+        // Call checkNumbers(a, b, c)
+
+        // Print result
+    }
+}
---
+++ src/Task26EnumDaysOfTheWeek.java
@@ -0,0 +1,5 @@
+// Write a Java program to create an enum called "DaysOfWeek" representing the days of the week.
+
+public class Task26DaysOfTheWeek
+{
+}
