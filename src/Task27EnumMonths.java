2929a50adbddddc952b42bc151ae86778b09471d
Isaac Alexander <Isaacalexander81@gmail.com>
2025-11-19 02:05:52 +0100

    src/Task27EnumMonths.java

--- src/Task27EnumMonths.java
+++ src/Task27EnumMonths.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+//Write a Java program that defines an enum called "Months" with constants representing the months of the year.
+
+
 
 public class Task27EnumMonths {
+
+    // Enum holding constant names of all 12 months
+    enum Months {
+        // JANUARY, FEBRUARY, ..., DECEMBER
+    }
+
+    public static void main(String[] args) {
+        // Assign a month to a variable
+        // Print selected month
+    }
 }
