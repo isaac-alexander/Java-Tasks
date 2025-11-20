3ec45d326849ac4e650c986c23258ef8f656cd28
Isaac Alexander <Isaacalexander81@gmail.com>
2025-11-20 01:58:05 +0100

    src/Task29PasswordValidator.java

--- src/Task29PasswordValidator.java
+++ src/Task29PasswordValidator.java
@@ -1,2 +1,32 @@
+// Write a Java method to check whether a string is a valid password.
+// Password rules:
+// A password must have at least ten characters.
+// A password consists of only letters and digits.
+// A password must contain at least two digits.
+
+//Expected Output:
+// 1. A password must have at least ten characters.
+// 2. A password consists of only letters and digits.
+// 3. A password must contain at least two digits
+//Input a password (You are agreeing to the above Terms and Conditions.): abcd1234xyz
+//Password is valid: abcd1234xyz
+
 public class Task29PasswordValidator {
+
+    // This method takes a String password and returns true or false
+    public static boolean isValidPassword(String password) {
+        // Check if password length is at least 10
+        // Loop through characters:
+        // Ensure each character is a letter or digit
+        // Count how many digits exist
+        // If digit count is at least 2, return true
+        // Otherwise return false
+        return false; // dummy return
+    }
+
+    public static void main(String[] args) {
+        // Password String
+        // Call isValidPassword()
+        // Print result
+    }
 }
