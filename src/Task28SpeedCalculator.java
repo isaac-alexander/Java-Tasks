02ff24177d42c88277ea68f4428213c5e7512498
Isaac Alexander <Isaacalexander81@gmail.com>
2025-11-20 01:57:50 +0100

    src/Task28SpeedCalculator.java

--- src/Task28SpeedCalculator.java
+++ src/Task28SpeedCalculator.java
@@ -1,6 +1,14 @@
+//Write a Java program to take the user for a distance (in meters) and the time taken (as three numbers: hours, minutes, seconds), and display the speed, in meters per second, kilometers per hour and miles per hour (hint: 1 mile = 1609 meters). Computer programming courses
+//Input distance in meters: 2500
+//Input hour: 5
+//Input minutes: 56
+//Input seconds: 23
+//Expected Output :
+//Your speed in meters/second is 0.11691531
+//Your speed in km/h is 0.42089513
+//Your speed in miles/h is 0.26158804
 
-
-public class Task28CalculateSpeed {
+public class Task28SpeedCalculator {
 
     // This method accepts distance in meters, and time in hours, minutes, seconds
     public static void calculateSpeed(double distance, int hours, int minutes, int seconds) {
