b33286c12544ce3afb910bbab0a822513ab9e424
Isaac Alexander <Isaacalexander81@gmail.com>
2025-11-20 01:58:19 +0100

    src/Task30CurrentDateTime.java

--- src/Task30CurrentDateTime.java
+++ src/Task30CurrentDateTime.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+//Write a Java method to display the current date and time.
+//Expected Output: Current date and time: Wednesday January 25, 2017 7:47:43
+
+
+
+// import java.text.SimpleDateFormat;
+// import java.util.Date;
 
 public class Task30CurrentDateTime {
+
+    // This method prints the current date and time
+    public static void displayDateTime() {
+        // Create a date format style (like Wednesday January 25, 2017 7:47:43)
+        // Create a Date object to get current time
+        // Print formatted date and time
+    }
+
+    public static void main(String[] args) {
+        // Call the displayDateTime method
+    }
 }
